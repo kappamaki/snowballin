21f90fe429a13a77eb30b0e55b0c4b3ae49cf569
kappamaki <nathmclean@gmail.com>
2014-09-07 12:09:04 -0700

    obstacle spawn limit, modify spawn chance based on speed, point system, bunny death sprite

--- img/sprites/bunny_die/resize_all.bat
+++ img/sprites/bunny_die/resize_all.bat
@@ -3,5 +3,5 @@
 set imgmagick="C:\Program Files (x86)\ImageMagick-6.7.6-Q16\convert.exe"
 
 for %%f in (*.png) DO (
-	%imgmagick% "%%f" -resize 72x72 -quality 100%% "half\%%f"
+	%imgmagick% "%%f" -resize 100x256 -quality 100%% "half\%%f"
 )
