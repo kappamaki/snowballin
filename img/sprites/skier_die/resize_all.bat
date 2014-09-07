851914854ff47abd354f5af420196bb0e7f1f0cb
kappamaki <nathmclean@gmail.com>
2014-09-07 16:30:58 -0700

    stuff

--- img/sprites/skier_die/resize_all.bat
+++ img/sprites/skier_die/resize_all.bat
@@ -3,5 +3,5 @@
 set imgmagick="C:\Program Files (x86)\ImageMagick-6.7.6-Q16\convert.exe"
 
 for %%f in (*.png) DO (
-	%imgmagick% "%%f" -resize 256x400 -quality 100%% "half\%%f"
+	%imgmagick% "%%f" -resize 320x460 -quality 100%% "half\%%f"
 )
